bfa1dd91ffa370b152ba10aa0542fbd759417c70
Edmundas <kasauskas.edmundas@gmail.com>
2023-12-04 19:15:16 +0200

    no clue if this gonna work

---
+++ js/carsDB.js
@@ -0,0 +1,54 @@
+$(function onDocReady() {
+    $('#addCarForm').submit(function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Call the callAPI function when the form is submitted
+        callAPI();
+    });
+});
+
+var callAPI = () => {
+    var carsDBModel = $('#carsDBModel').val();
+    var carsDBLicensePlate = $('#carsDBLicensePlate').val();
+    var carsDBGearbox = $('#carsDBGearbox').val();
+    var carsDBReleaseYear = $('#carsDBReleaseYear').val();
+    var carsDBInsuranceDate = $('#carsDBInsuranceDate').val();
+    var carsDBTechDate = $('#carsDBTechDate').val();
+    var carsDBPower = $('#carsDBPower').val();
+    var carsDBdrivebase = $('#carsDBdrivebase').val();
+
+    // Create a JSON object with the variables
+    var requestData = {
+        carsDBModel: carsDBModel,
+        carsDBLicensePlate: carsDBLicensePlate,
+        carsDBGearbox: carsDBGearbox,
+        carsDBReleaseYear: carsDBReleaseYear,
+        carsDBInsuranceDate: carsDBInsuranceDate,
+        carsDBTechDate: carsDBTechDate,
+        carsDBPower: carsDBPower,
+        carsDBdrivebase: carsDBdrivebase
+    };
+
+    // Convert the JSON object to a string
+    var raw = JSON.stringify(requestData);
+
+    // Instantiate a Headers object
+    var myHeaders = new Headers();
+    // Add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+
+    // Create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    // Make API call with parameters and use promises to get response
+    fetch("https://x10uh2luad.execute-api.eu-west-1.amazonaws.com/alpha", requestOptions)
+        .then(response => response.text())
+        .then(result => alert(JSON.parse(result).body))
+        .catch(error => console.log('error', error));
+};
