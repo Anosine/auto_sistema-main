87f264d82e09f22ce27ad52d9c561854ae400a3a
Edmundas <kasauskas.edmundas@gmail.com>
2023-12-05 16:42:03 +0200

    try read

--- js/carsDB.js
+++ js/carsDB.js
@@ -1,5 +1,6 @@
 $(function onDocReady() {
     $('#addCarForm').submit(callAPI)
+    $('#getAllCarsButton').click(getAllCars);
     });
 
 function callAPI(event)  {
@@ -47,4 +48,58 @@ function callAPI(event)  {
         .then(response => response.text())
         .then(result => alert(JSON.parse(result).body))
         .catch(error => console.log('error', error));
+};
+
+var getAllCars = () => {
+    // Instantiate a Headers object
+    var myHeaders = new Headers();
+    // Add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+
+    // Create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'GET', // Use GET method for retrieving data
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    // Make API call to get all cars and use promises to handle the response
+    fetch("https://x10uh2luad.execute-api.eu-west-1.amazonaws.com/alpha", requestOptions)
+        .then(response => response.json()) // Assuming the response is in JSON format
+        .then(result => {
+            // Process the result (array of cars)
+            console.log(result);
+            
+            // Update the container element with the dynamic table
+            updateTable(result);
+        })
+        .catch(error => console.log('error', error));
+};
+
+var updateTable = (cars) => {
+    // Assuming you have a container element with ID 'tableContainer'
+    var tableContainer = $('#tableContainer');
+
+    // Create a dynamic table
+    var tableHTML = '<table border="1">';
+    tableHTML += '<tr><th>Model</th><th>License Plate</th><th>Gearbox</th><th>Release Year</th><th>Insurance Date</th><th>Tech Date</th><th>Power</th><th>Drivebase</th></tr>';
+
+    // Iterate through the cars array and add rows to the table
+    cars.forEach(car => {
+        tableHTML += '<tr>';
+        tableHTML += '<td>' + car.carsDBModel + '</td>';
+        tableHTML += '<td>' + car.carsDBLicensePlate + '</td>';
+        tableHTML += '<td>' + car.carsDBGearbox + '</td>';
+        tableHTML += '<td>' + car.carsDBReleaseYear + '</td>';
+        tableHTML += '<td>' + car.carsDBInsuranceDate + '</td>';
+        tableHTML += '<td>' + car.carsDBTechDate + '</td>';
+        tableHTML += '<td>' + car.carsDBPower + '</td>';
+        tableHTML += '<td>' + car.carsDBdrivebase + '</td>';
+        tableHTML += '</tr>';
+    });
+
+    tableHTML += '</table>';
+
+    // Update the content of the container element
+    tableContainer.html(tableHTML);
 };
